e0379b23ce31d4d8bdb11bdbb9c685e5c44dc845
mohamadarban4 <84586061+mohamadarban4@users.noreply.github.com>
2024-04-05 22:28:46 +0530

    Add files via upload

---
+++ public/script.js
@@ -0,0 +1,20 @@
+// gsap.to("#coo", {
+//     duration: 0.5,
+//     scale: 1.1,
+//     ease: "power1.inOut",
+//     repeat: -1,
+//     yoyo: true,
+//     // Add more properties as needed for your effect
+//     // You can customize the animation properties according to your requirement
+//     // For example, you can add rotation, opacity changes, etc.
+//     // Refer to GSAP documentation for more options: https://greensock.com/docs/v3/GSAP
+//     // Feel free to modify the animation properties to achieve the desired effect
+//     // Here, the image will scale up and down continuously on hover
+//     // You can modify these properties to create a "magnet" effect
+//     // For magnet effect, you may want to adjust the scaleX and scaleY properties instead of scale
+// });
+gsap.from("#pu",{
+    duration:1,
+    x:60,
+    delay:0.8,
+})
